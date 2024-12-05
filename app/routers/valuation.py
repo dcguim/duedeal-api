7a6e73cc34dc58c66171cb76d0c70cd05253b2fb
dcguim <dcguim@gmail.com>
2024-12-05 15:32:29 -0300

    Add import for sqlalchemy sessionmaker

--- app/routers/valuation.py
+++ app/routers/valuation.py
@@ -2,6 +2,7 @@ import json
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from app.model.users import Waitlist
 from email_validator import validate_email, EmailNotValidError
 
