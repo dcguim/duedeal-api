03fdb58e7f25b3de4963968ac6ccd68ddacb142a
dcguim <dcguim@gmail.com>
2024-12-09 10:09:01 -0300

    Add CORS middleware for javascript based requests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.routers import valuation
-from app.model.users import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(valuation.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3006"],  # React's development server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
